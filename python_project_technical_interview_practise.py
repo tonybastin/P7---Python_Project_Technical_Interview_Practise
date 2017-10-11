a9557b41accb36405abbcc644dc4512956e0582f
Tony Bastin <tonyship30@gmail.com>
2017-10-10 22:19:01 -0400

    feat: Add solution to question 2

--- python_project_technical_interview_practise.py
+++ python_project_technical_interview_practise.py
@@ -12,3 +12,29 @@ def question1(s,t):
     if count_t == len(t):
         return True
     return False   
+    
+'''Question 2
+    Given a string a, find the longest palindromic substring contained in a. 
+    Your function definition should look like question2(a), and return a 
+    string.
+'''
+
+def helper_palindrome(word):
+    count = len(word) - 1
+    for i in word:
+        if i.lower() != word[count].lower():
+            return ""
+        count -=1
+    return word 
+
+def question2(a):
+    palindrome = ""
+    for i in range(len(a)-1):        
+        if len(helper_palindrome(a[i:]))>len(palindrome):
+            palindrome = helper_palindrome(a[i:])
+        if len(helper_palindrome(a[:i+1]))>len(palindrome):
+            palindrome = helper_palindrome(a[:i+1])
+        
+    return palindrome
+    
+    
