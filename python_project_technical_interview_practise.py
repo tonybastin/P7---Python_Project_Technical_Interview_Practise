fd2abf22e095a8e6221ba7acf860b754f829d863
Tony Bastin <tonyship30@gmail.com>
2017-10-11 23:08:52 -0400

    test: Add test method for question 1

--- python_project_technical_interview_practise.py
+++ python_project_technical_interview_practise.py
@@ -3,6 +3,8 @@
     function returns True. Your function definition should look like: 
     question1(s, t) and return a boolean True or False.
 '''
+
+# Find out if s1 and s2 strings are anagram of each other
 def question1(s,t):
     count_t = 0
     for i in t:
@@ -11,8 +13,41 @@ def question1(s,t):
             
     if count_t == len(t):
         return True
-    return False   
-    
+    return False 
+
+# Main program
+def main():
+    print (question1("udacity", "ciy"))
+
+if __name__ == '__main__':
+    main()  
+
+
+"""
+The test cases for function question1(s,t) is given below
+
+Case 1: question1("udacity", "ad") -> True
+string with 2 characters reversed from order in S
+
+Case 2: question1("udacity", "da") -> True
+string with 2 characters same as in order in S
+
+Case 3: ("udacity, acity") -> True
+string with 6 characters same as in order in S
+
+Case 4: ("udacity", "ciy") -> False
+string with 2 characters same as in order in S and 1 character out of order.
+
+Case 5: ("udacity", "xyz") -> False
+string with new characters not in string S.
+
+Case 6: ("udacity", "") -> True
+empty string.
+
+Case 7: ("ad", "udacity" ) -> False
+string t's length is greater than string S.
+""" 
+  
 '''Question 2
     Given a string a, find the longest palindromic substring contained in a. 
     Your function definition should look like question2(a), and return a 
