008d5d44d2192e23f830fbdcabf6b2cbba51f78e
Tony Bastin <tonyship30@gmail.com>
2017-10-14 15:16:43 -0400

    fix: Add additional checks for question 1
    The checks include null value and extreme value cases

---
+++ solutions.py
@@ -0,0 +1,38 @@
+''' Question 1 Given two strings s and t, determine whether some anagram of t 
+    is a substring of s. For example: if s = "udacity" and t = "ad", then the 
+    function returns True. Your function definition should look like: 
+    question1(s, t) and return a boolean True or False.
+'''
+
+def question1(s,t):
+    # convert 's' and 't' to lower case
+    s = s.lower()
+    t = t.lower()
+    
+    # check if length of 's' is lower than 't'
+    if len(s) < len(t):
+        return False
+    
+    # split 's' into substrings having length of 't'
+    substrings = []
+    for i in range(len(s)-len(t)+1):        
+        substrings.append(s[i:i+len(t)])
+    
+    # for each substring check whether all letters in 't' 
+    # excist in 's' and return 'True' if found   
+    for substring in substrings:
+        count_substring = 0
+        for i in t:
+            if i in substring:
+                count_substring += 1                    
+        if count_substring == len(t):
+            return True
+    return False        
+
+ 
+  
+'''Question 2
+    Given a string a, find the longest palindromic substring contained in a. 
+    Your function definition should look like question2(a), and return a 
+    string.
+'''
