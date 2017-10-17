bb61158178c0d5a038cbdab3dd3f8196ea1a0286
Tony Bastin <tonyship30@gmail.com>
2017-10-17 08:59:08 -0400

    fix: Improve the time effeciency for q2

--- solutions.py
+++ solutions.py
@@ -41,36 +41,35 @@ print (question1("", ""))
     Your function definition should look like question2(a), and return a 
     string.
 '''
-def helper_palindrome(word):
-    # This function checks if a given word is palindrome
-    count = len(word) - 1
-    for i in word:
-        if i.lower() != word[count].lower():
-            return ""
-        count -=1
-    return word 
-
 def question2(a):
-    palindrome = ""
+    # Implementing Manacher's algorithm
     
     # is 'a' is either "" or None return "Enter a valid input"
     if a == "" or a == None or a == " ":
         return "Please enter a valid string"
     
-    # splits the given sting into substings
-    for i in range(len(a)):        
-        for j in range(i+1,len(a)+1):
-            substring = a[i:j]
-            if helper_palindrome(substring) and substring != " ":
-                if len(substring) > len(palindrome):
-                    palindrome = substring
-                
-    # Incase a palindrome is identified with first and last positions as " ", 
-    # it willbe removed
-    if palindrome[0] == " ":
-        palindrome = palindrome[1:-1]
-        
-    return palindrome
+    # a trick that simplifies the problems
+    # e.g.:"aaa" ->"$#a#a#a#" or "aa"->"$#a#a#"
+    s = '$#' + '#'.join(a) + '##'
+    P = [0] * len(s)
+    Mirror,C,R,maxLPS,maxLPSIndex = 0,0,0,0,0
+    for i in range(1,len(s)-1):
+        Mirror = 2*C - i 
+        if i < R:
+            P[i] =  min(R-i,P[Mirror])
+        while s[i + (P[i] + 1)] == s[i - (P[i] + 1)]:
+            P[i] += 1
+        if i + P[i] > R:
+            C = i
+            R = i + P[i]
+        # Keep track of the index at which the longest palindromic 
+        # substring occurs
+        if P[i] > maxLPS:
+            maxLPS = P[i]
+            maxLPSIndex = i
+    #print (P)
+    return ( (s[maxLPSIndex - P[maxLPSIndex]: maxLPSIndex + 1 \
+                + P[maxLPSIndex]].replace('#','')))
 
 
 print ("\nPrinting results for question 2 :\n")
